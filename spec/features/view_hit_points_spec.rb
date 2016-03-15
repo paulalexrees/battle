5a29ccd8f5a4841227fb063090fbc1e9f8b62e1b
Paul <paulalexrees@gmail.com>
2016-03-15 14:52:55 +0000

    Challenge 21 done - helpers

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "Viewing Hitpoints" do
   scenario "As Player one" do
-    visit "/"
-    fill_in :player_1_name, with: "Tobenna"
-    fill_in :player_2_name, with: "Mittens"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_text "Mittens: 20HP"
   end
 end
